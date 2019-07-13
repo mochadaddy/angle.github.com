8f35aa1446a0af2e0f3a18f4088a583a6e3759a0
mochadaddy <netmonk@sina.com>
2019-07-13 15:48:29 +0800

    周线标志位计算

--- stock_week_price.py
+++ stock_week_price.py
@@ -4,38 +4,31 @@ import pandas as pd
 import os
 import csv
 import stock_class as sc
-
-
-def find_win_team(df_new):
-    winners = []
-    for i, row in df.iterrows():
-        if row['score_A'] > row['score_B']:
-            winners.append(row['team_A'])
-        elif row['score_A'] < row['score_B']:
-            winners.append(row['team_B'])
-        else:
-            winners.append('Draw')
-    return winners
-
+import numpy as np
 
 
 # 联接tushare的api接口
 pro = sc.get_tocken()
 yestorday = sc.get_sys_date()
 targetdir = 'D:/Program Files/tdx/vipdoc/sz/sz_week'
-df = ts.pro_bar(ts_code='000001.SZ', freq='W', adj='hfq', start_date='19910101', end_date=yestorday, ma=[5])
-df_aes = df.sort_values('trade_date', ascending=True)
-df_new = df_aes.reset_index(drop=True)
-df_new['flag'] = None
-row_num = df_new.shape[0]
-print df_new
+read_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_nhsy/nhsy.csv'
 
-for i in range(0, row_num):
-    if df_new.iloc[i]['close'] >= df_new.iloc[i]['ma5']:
-        df_new.iloc[i]['flag'] = 1
-    if df_new.iloc[i]['close'] < df_new.iloc[i]['ma5']:
-        df_new.iloc[i]['flag'] = 0
+stock_list = pd.read_csv(read_dir, usecols=['ts_code', 'year_annual_return'])
+stock_list_5 = stock_list[stock_list['year_annual_return'] >= 10]
+#print stock_list_5
+
+stock_num = stock_list_5.shape[0]
+for i in range(0, stock_num):
+    stock_code = stock_list.iloc[i]['ts_code']
+    df = ts.pro_bar(ts_code=stock_code, freq='W', adj='hfq', start_date='19910101', end_date=yestorday, ma=[5])
+    df_aes = df.sort_values('trade_date', ascending=True)
+    df_new = df_aes.reset_index(drop=True)
+    # 当周线收盘价高于5周均线时，置flag标志位为1，当周线收盘价低于5周均线时，置flag标志位为0
+    df_new['flag'] = ''
+    df_new['return'] = ''
+    mask = df_new['close'] - df_new['ma5']
+    df_new.loc[mask > 0, 'flag'] = 1
+    df_new.loc[mask < 0, 'flag'] = 0
+    pd.DataFrame.to_csv(df_new, targetdir + os.sep + stock_code + '.csv', encoding='gbk')
 
 
-
-pd.DataFrame.to_csv(df_new, targetdir + os.sep + '000001.csv', encoding='gbk')
