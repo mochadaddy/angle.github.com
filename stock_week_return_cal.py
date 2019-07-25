9eb86708bc1f38632e6ec256fbdf31d46310357d
mochadaddy <netmonk@sina.com>
2019-07-25 09:12:49 +0800

    增加计算最大回撤比例功能

--- stock_week_return_cal.py
+++ stock_week_return_cal.py
@@ -3,11 +3,18 @@ import tushare as ts
 import pandas as pd
 import numpy as np
 import os
+import glob
 import csv
 import stock_class as sc
 
 target_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_week_return'
 read_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_week_fill'
+fileNames_week = glob.glob(target_dir + r'\*')
+for fileName in fileNames_week:
+    try:
+        os.remove(fileName)
+    except:
+        break
 m = n = 0
 read_dir_files = os.listdir(read_dir)
 for stock_file in read_dir_files:
@@ -41,6 +48,8 @@ for stock_file in read_dir_files:
                 if k == 1:
                     df.ix[i, 'money_cal'] = df.ix[i, 'close']
                     buy_price_init = df.ix[i, 'close']
+                    max_price = buy_price_init
+                    max_withdrawal_rate = 0
             if df.iloc[i - 1]['flag'] == 1 and df.iloc[i]['flag'] == 0:
                 if buy_price is None:
                     continue
@@ -52,23 +61,21 @@ for stock_file in read_dir_files:
                     while pd.isnull(df.iloc[j]['money_cal']):
                         j = j - 1
                     df.ix[i, 'money_cal'] = df.ix[j, 'money_cal'] * return_rate + df.ix[j, 'money_cal']
-                    '''
-                    if k == 1:
-                        max_price = buy_price_init
-                        if df.ix[i, 'money_cal'] > max_price:
-                            max_price = df.ix[i, 'money_cal']
-                    if k == 1:
-                        withdrawal_rate = 1 - (df.ix[i, 'money_cal']/max_price)
-                    max_withdrawal_rate = withdrawal_rate
-                    if max_withdrawal_rate > withdrawal_rate:
-                    '''
-
+                    # 取得当前价格之前的最高价格
+                    if df.ix[i, 'money_cal'] > max_price:
+                        max_price = df.ix[i, 'money_cal']
+                    # 计算最大回撤比例
+                    withdrawal_rate = 1 - (df.ix[i, 'money_cal'] / max_price)
+                    # 比较所有的回撤比例，取得最大的回撤比例
+                    if max_withdrawal_rate < withdrawal_rate:
+                        max_withdrawal_rate = withdrawal_rate
                     times = round((df.ix[i, 'money_cal']-buy_price_init)/buy_price_init/year_num, 2)
                     times_no_ma5 = round((df.ix[i, 'close']-buy_price_init)/buy_price_init/year_num, 2)
-    print (stock_file, times, times_no_ma5)
+    print (stock_file, times, times_no_ma5, max_withdrawal_rate)
     if times > times_no_ma5:
         m = m + 1
     else:
         n = n + 1
     print (m, n)
     pd.DataFrame.to_csv(df, target_dir + os.sep + stock_file, encoding='gbk')
+    
