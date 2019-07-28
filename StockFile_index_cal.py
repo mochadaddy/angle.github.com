619a7aa8b5d5c138b14f1bfb5b72c05824c07839
mochadaddy <netmonk@sina.com>
2019-07-28 20:29:27 +0800

    更新数据下载增加周线数据下载

--- StockFile_index_cal.py
+++ StockFile_index_cal.py
@@ -11,31 +11,34 @@ import stock_class as sc
 
 # 联接tushare的api接口
 pro = sc.get_tocken()
-targetdir = 'D:/Program Files/tdx/vipdoc/sz/sz_tushare/'
+targetdir = 'D:/Program Files/tdx/vipdoc/sz/sz_tushare/day_download'
+targetdir_week = 'D:/Program Files/tdx/vipdoc/sz/sz_tushare/week_download'
 startdate = '19910101'
 # 得到系统日期的前一天
-formatted_trade_date = sc.get_sys_date()
+formatted_yesterday = sc.get_sys_date()
 ma_list = [5, 10, 250]
 stock_list = sc.get_sz_stock()
 # print stock_list
 stock_num = stock_list.shape[0]
 
-for i in range(2044, stock_num):
+
+for i in range(0, stock_num):
 
     stock_code = stock_list.iloc[i]['ts_code']
     #print stock_code
     # print(ts.get_hist_data('000001', start='1991-01-01', end='2019-01-01', ))
 
-    df = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=startdate, end_date=formatted_trade_date)
+    df = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=startdate, end_date=formatted_yesterday)
+
     # print type(df)
     rownum = df.shape[0]
     # print rownum
     if rownum == 4000:
-        enddate_mid = int(int(startdate) + int(formatted_trade_date))/2
+        enddate_mid = int(int(startdate) + int(formatted_yesterday))/2
         enddate_mid_str = str(enddate_mid)
         df1 = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=startdate, end_date=enddate_mid_str)
 
-        df2 = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=enddate_mid_str, end_date=formatted_trade_date)
+        df2 = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=enddate_mid_str, end_date=formatted_yesterday)
         frames = [df2, df1]
         frames_result = pd.concat(frames)
         df = frames_result.sort_values('trade_date', ascending=True)
@@ -52,11 +55,9 @@ for i in range(2044, stock_num):
         df_new = df.reset_index(drop=True)
         pd.DataFrame.to_csv(df_new, targetdir + os.sep + stock_code + '.csv', encoding='gbk')
 
+    df_week = ts.pro_bar(ts_code=stock_code, freq='W', adj='hfq', start_date=startdate, end_date=formatted_yesterday,
+                         ma=[5])
+    df_week_sort = df_week.sort_values('trade_date', ascending=True)
+    pd.DataFrame.to_csv(df_week_sort, targetdir_week + os.sep + stock_code + '.csv', encoding='gbk')
     #pd.DataFrame.to_csv(df, targetdir + os.sep + '000001.csv',  encoding='gbk')
-
-
-
-
-
-
 
