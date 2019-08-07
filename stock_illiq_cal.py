afc0937a04392b058ceee07673c8bb53773a9ed7
mochadaddy <netmonk@sina.com>
2019-08-07 17:03:17 +0800

    非流动因子计算
    拆分功能

--- stock_illiq_cal.py
+++ stock_illiq_cal.py
@@ -23,51 +23,6 @@ end_date = '20190630'
 stock_list = sc.get_return_rate(0)
 stock_num = stock_list.shape[0]
 
-'''
-fileNames_week = glob.glob(target_dir + r'\*')
-for fileName in fileNames_week:
-    try:
-        os.remove(fileName)
-    except:
-        break
-for j in range(0, stock_num):
-    stock_code = stock_list.iloc[j]['ts_code']
-    df = pd.read_csv(read_dir + os.sep + stock_code + '.csv', usecols=['ts_code', 'trade_date', 'open', 'close', 'change',
-                                                                   'pct_chg', 'vol', 'amount', 'MA_5', 'MA_10', 'MA_250'])
-    row_num = df.shape[0]
-    #count = 100
-    df_stock_illiq = pd.DataFrame(columns=['ts_code', 'trade_date', 'illiq', 'illiq' + str(5)])
-    for i in range(0, row_num):
-        if str(df.iloc[i]['trade_date']) >= start_date and str(df.iloc[i]['trade_date']) <= end_date:
-            #print df.ix[i, :]
-            df_stock_illiq.at[i, 'ts_code'] = df.iloc[i]['ts_code']
-            df_stock_illiq.at[i, 'trade_date'] = df.iloc[i]['trade_date']
-            illiq = abs(df.iloc[i]['change'])*1.0/df.iloc[i]['amount']*10000
-# coding: utf-8
-'''
-按照日线计算emv指标和maemv指标
-'''
-import tushare as ts
-import pandas as pd
-import os
-import time
-from datetime import datetime as dt
-import datetime as d
-import glob
-import csv
-import stock_class as sc
-import numpy as np
-
-pro = sc.get_tocken()
-read_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_tushare/day_download'
-target_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_illiq'
-read_dir_illiq = 'D:/Program Files/tdx/vipdoc/sz/sz_illiq'
-target_dir_illiq = 'D:/Program Files/tdx/vipdoc/sz/sz_illiq_compare'
-start_date = '20190301'
-end_date = '20190630'
-stock_list = sc.get_return_rate(0)
-stock_num = stock_list.shape[0]
-
 
 fileNames_week = glob.glob(target_dir + r'\*')
 for fileName in fileNames_week:
@@ -107,4 +62,4 @@ for stock_file in szlistfile:
     df_count = df_count.append(df_illiq_transpotion)
 pd.DataFrame.to_csv(df_count, target_dir_illiq + os.sep + 'illiq.csv', encoding='gbk')
 
-        
+    
