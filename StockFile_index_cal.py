a6e812c4ea488349eedbb00a832fc5c7da3dee92
mochadaddy <netmonk@sina.com>
2019-08-08 13:44:49 +0800

    日线周线数据下载
    更新超时机制

--- StockFile_index_cal.py
+++ StockFile_index_cal.py
@@ -25,7 +25,7 @@ stock_num = stock_list.shape[0]
 df = pd.DataFrame()
 df_week = pd.DataFrame()
 
-for i in range(280, stock_num):
+for i in range(281, stock_num):
 
     stock_code = stock_list.iloc[i]['ts_code']
     #print stock_code
@@ -67,8 +67,10 @@ for i in range(280, stock_num):
                 df['MA_' + str(ma)] = df['close'].rolling(window=ma, center=False, ).mean()
             df_new = df.reset_index(drop=True)
             pd.DataFrame.to_csv(df_new, targetdir + os.sep + stock_code + '.csv', encoding='gbk')
-
-        df_week_sort = df_week.sort_values('trade_date', ascending=True)
+        if df_week is None:
+            continue
+        else:
+            df_week_sort = df_week.sort_values('trade_date', ascending=True)
         pd.DataFrame.to_csv(df_week_sort, targetdir_week + os.sep + stock_code + '.csv', encoding='gbk')
     #pd.DataFrame.to_csv(df, targetdir + os.sep + '000001.csv',  encoding='gbk')
 '''
