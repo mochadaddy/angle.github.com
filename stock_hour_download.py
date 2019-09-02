4b952f934bc3bfb750fbb31f338e1f3bd175ec82
mochadaddy <netmonk@sina.com>
2019-09-02 23:25:26 +0800

    小时数据下载更新

--- stock_hour_download.py
+++ stock_hour_download.py
@@ -6,6 +6,7 @@ import stock_class as sc
 import time
 import datetime as dt
 from datetime import datetime as dtt
+from goto import with_goto
 
 '''
 #通过 tushare的stock_basic、pro_bar、daily_basic接口计算股票总市值、流通市值
@@ -26,26 +27,39 @@ stock_list = sc.get_sz_stock()
 stock_num = stock_list.shape[0]
 
 df_hour = pd.DataFrame()
-
 time_format = dtt.strptime(formatted_yesterday, '%Y%m%d')
 weekday = time_format.weekday() + 1
+
+@with_goto
+def func(stock_code):
+    df_hour = pd.DataFrame()
+    label.begin
+    for _ in range(3):
+        try:
+            #df = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=startdate, end_date=formatted_yesterday)
+            df_hour = ts.pro_bar(ts_code=stock_code, adj='hfq', freq='60min', start_date='20190731', end_date='20190831')
+
+        except:
+            time.sleep(2)
+
+        if df_hour is None or df_hour.empty:
+            goto.begin
+        return df_hour
+
 
 for i in range(0, stock_num):
 
     stock_code = stock_list.iloc[i]['ts_code']
     # print stock_code
     # print(ts.get_hist_data('000001', start='1991-01-01', end='2019-01-01', ))
-
-    for _ in range(3):
-        try:
-            #df = ts.pro_bar(ts_code=stock_code, adj='hfq', start_date=startdate, end_date=formatted_yesterday)
-            df_hour = ts.pro_bar(ts_code=stock_code, adj='hfq', freq='60min', start_date='20190731', end_date='20190831')
+    #df_hour = ts.pro_bar(ts_code=stock_code, adj='hfq', freq='60min', start_date='20190731', end_date='20190831')
+    df_hour_return = func(stock_code)
 
 
-        except:
-            time.sleep(2)
-    if df_hour is None:
-        continue
 
-    pd.DataFrame.to_csv(df_hour, target_dir + os.sep + stock_code + '.csv', encoding='gbk')
+    pd.DataFrame.to_csv(df_hour_return, target_dir + os.sep + stock_code + '.csv', encoding='gbk')
+    df_hour.drop(df_hour.index, inplace=True)
+
+
+
 
