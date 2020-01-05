90ca34dbc75e554f2ec395115f44604741d833b2
mochadaddy <netmonk@sina.com>
2020-01-06 07:19:20 +0800

    rsrs买入卖出计算

--- stock_rsrs_historical_back_test.py
+++ stock_rsrs_historical_back_test.py
@@ -13,34 +13,37 @@ import numpy as np
 
 read_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_rsrs_beta_cal'
 targetdir = 'D:/Program Files/tdx/vipdoc/sz/sz_rsrs_flag_add'
-df = pd.read_csv(read_dir + os.sep + 'beta_cal.csv', usecols=['ts_code', 'trade_date', 'high', 'low', 'close', 'beta'])
 
-row_num = df.shape[0]
-df['flag'] = None
-for i in range(0, row_num):
+read_dir_files = os.listdir(read_dir)
+for stock_file in read_dir_files:
+    df = pd.read_csv(read_dir + os.sep + stock_file, usecols=['ts_code', 'trade_date', 'high', 'low', 'close', 'beta'])
 
-    if i > 0:
-        if (df.iloc[i]['beta'] >= 1) and (df.iloc[i-1]['beta'] < 1):
+    row_num = df.shape[0]
+    df['flag'] = None
+    for i in range(0, row_num):
 
-            k = i
-            while pd.isnull(df.iloc[k]['flag']):
-                k = k -1
-                if df.iloc[k]['flag'] == 0 or k == 0:
-                    df.at[i, 'flag'] = 1
-                    break
-                elif df.iloc[k]['flag'] == 1:
-                    break
-        if (df.iloc[i]['beta'] <= 0.8) and (df.iloc[i-1]['beta'] > 0.8):
-            j = i
-            while pd.isnull(df.iloc[j]['flag']):
-                j = j - 1
-                if df.iloc[j]['flag'] == 1:
-                    df.at[i, 'flag'] = 0
-                else:
-                   continue
-    else:
-        continue
-pd.DataFrame.to_csv(df, targetdir + os.sep + 'beta_flag.csv', encoding='gbk')
+        if i > 0:
+            if (df.iloc[i]['beta'] >= 1) and (df.iloc[i-1]['beta'] < 1):
+
+                k = i
+                while pd.isnull(df.iloc[k]['flag']):
+                    k = k -1
+                    if df.iloc[k]['flag'] == 0 or k == 0:
+                        df.at[i, 'flag'] = 1
+                        break
+                    elif df.iloc[k]['flag'] == 1:
+                        break
+            if (df.iloc[i]['beta'] <= 0.85) and (df.iloc[i-1]['beta'] > 0.85):
+                j = i
+                while pd.isnull(df.iloc[j]['flag']) and j >= 0:
+                    j = j - 1
+                    if df.iloc[j]['flag'] == 1:
+                        df.at[i, 'flag'] = 0
+                    else:
+                       continue
+        else:
+            continue
+    pd.DataFrame.to_csv(df, targetdir + os.sep + stock_file, encoding='gbk')
 
 
 
