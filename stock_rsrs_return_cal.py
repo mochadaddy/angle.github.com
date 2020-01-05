2d42dd134a92791d4ea849805967c158599f0f8c
mochadaddy <netmonk@sina.com>
2020-01-06 07:20:05 +0800

    rsrs收益率计算

--- stock_rsrs_return_cal.py
+++ stock_rsrs_return_cal.py
@@ -11,29 +11,32 @@ import glob
 
 read_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_rsrs_flag_add'
 target_dir = 'D:/Program Files/tdx/vipdoc/sz/sz_rsrs_return'
-df = pd.read_csv(read_dir + os.sep + 'beta_flag.csv',
-                 usecols=['ts_code', 'trade_date', 'high', 'low', 'close', 'beta', 'flag'])
-row_num = df.shape[0]
-buy_price = None
-year_num = round(row_num/250, 2)
-k = 0
-for i in range(0, row_num):
-    if np.isnan(df.iloc[i]['flag']):
-        continue
-    else:
-        if df.iloc[i]['flag'] == 1:
-            buy_price = df.iloc[i]['close']
-            k = k + 1
-            if k == 1:
-                df.ix[i, 'money_cal'] = df.ix[i, 'close']
-                buy_price_init = df.ix[i, 'close']
-        if df.iloc[i]['flag'] == 0:
-            sell_price = df.iloc[i]['close']
-            return_rate = (sell_price - buy_price) / buy_price
-            df.ix[i, 'return_rate'] = return_rate
-            j = i
-            while pd.isnull(df.iloc[j]['money_cal']):
-                j = j - 1
-            df.ix[i, 'money_cal'] = df.ix[j, 'money_cal'] * return_rate + df.ix[j, 'money_cal']
-pd.DataFrame.to_csv(df, target_dir + os.sep + 'rsrs_return.csv', encoding='gbk')
+
+read_dir_files = os.listdir(read_dir)
+for stock_file in read_dir_files:
+    df = pd.read_csv(read_dir + os.sep + stock_file,
+                     usecols=['ts_code', 'trade_date', 'high', 'low', 'close', 'beta', 'flag'])
+    row_num = df.shape[0]
+    buy_price = None
+    year_num = round(row_num/250, 2)
+    k = 0
+    for i in range(0, row_num):
+        if np.isnan(df.iloc[i]['flag']):
+            continue
+        else:
+            if df.iloc[i]['flag'] == 1:
+                buy_price = df.iloc[i]['close']
+                k = k + 1
+                if k == 1:
+                    df.ix[i, 'money_cal'] = df.ix[i, 'close']
+                    buy_price_init = df.ix[i, 'close']
+            if df.iloc[i]['flag'] == 0:
+                sell_price = df.iloc[i]['close']
+                return_rate = (sell_price - buy_price) / buy_price
+                df.ix[i, 'return_rate'] = return_rate
+                j = i
+                while pd.isnull(df.iloc[j]['money_cal']):
+                    j = j - 1
+                df.ix[i, 'money_cal'] = df.ix[j, 'money_cal'] * return_rate + df.ix[j, 'money_cal']
+    pd.DataFrame.to_csv(df, target_dir + os.sep + stock_file, encoding='gbk')
 
